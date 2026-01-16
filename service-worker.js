c627d6fea479a6de1616fecff7c5572963d69153
RiseATLAS <petterelgvin@gmail.com>
2026-01-16 21:56:26 +0100

    Updates

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'booksWithMusic-v2';
+const AUDIO_CACHE = 'booksWithMusic-audio-v2';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/BooksWithMusic/',
+        '/BooksWithMusic/index.html',
+        '/BooksWithMusic/styles.css',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key.startsWith('booksWithMusic-') && key !== CACHE_NAME && key !== AUDIO_CACHE)
+          .map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/audio/')) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  }
+});
